518b2a83daea495aac10b777bbcfb3eff96f1030
cristito <cdpsk8@gmail.com>
2025-03-19 16:25:09 -0500

    produccion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import ProductCarousel from "@/components/ProductCarousel";
 import { Product } from "@/app/types"; // Asegúrate de importar el tipo Product
 
-async function getProducts(): Promise<Product[]> {
+/* async function getProducts(): Promise<Product[]> {
   const res = await fetch("http://localhost:3000/api");
 
   if (!res.ok) {
@@ -9,15 +9,15 @@ async function getProducts(): Promise<Product[]> {
   }
 
   return res.json();
-}
+} */
 
 export default async function Home() {
-  const products = await getProducts();
-
+/*   const products = await getProducts();
+ */
   return (
     <div className="flex flex-col items-center">
       <h1 className="text-3xl font-bold text-center my-8">Productos Destacados</h1>
-      <ProductCarousel products={products} />
+      {/* <ProductCarousel products={products} /> */}
     </div>
   );
 }

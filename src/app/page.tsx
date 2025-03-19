f473abe1cc55c68a350984635778ca88593a4f85
cristito <cdpsk8@gmail.com>
2025-03-19 16:27:44 -0500

    produccionnnnn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import ProductCarousel from "@/components/ProductCarousel";
-import { Product } from "@/app/types"; // Asegúrate de importar el tipo Product
+/* import ProductCarousel from "@/components/ProductCarousel";
+import { Product } from "@/app/types"; // Asegúrate de importar el tipo Product */
 
 /* async function getProducts(): Promise<Product[]> {
   const res = await fetch("http://localhost:3000/api");

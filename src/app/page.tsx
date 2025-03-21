0348cf7aaaaee96535c0fc48fbf7f6e6edb0ded1
cristito <cdpsk8@gmail.com>
2025-03-21 17:08:32 -0500

    netfly

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,25 @@
-/* import ProductCarousel from "@/components/ProductCarousel";
-import { Product } from "@/app/types"; // Asegúrate de importar el tipo Product */
+import ProductCarousel from "@/components/ProductCarousel";
+import { Product } from "@/app/types"; // Asegúrate de importar el tipo Product
 
-/* async function getProducts(): Promise<Product[]> {
-  const res = await fetch("http://localhost:3000/api");
+async function getProducts2(): Promise<Product[]> {
+  const url = process.env.urlLocal
+  const res = await fetch(url+'/api');
+  
 
   if (!res.ok) {
     throw new Error("Error al obtener los productos");
   }
 
   return res.json();
-} */
+}
 
 export default async function Home() {
-/*   const products = await getProducts();
- */
+  const products = await getProducts2();
+
   return (
     <div className="flex flex-col items-center">
       <h1 className="text-3xl font-bold text-center my-8">Productos Destacados</h1>
-      {/* <ProductCarousel products={products} /> */}
+      <ProductCarousel products={products} />
     </div>
   );
 }
